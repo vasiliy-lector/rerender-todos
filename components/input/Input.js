338f33420ddf521468b1f84c5ec15a216b537777
Vasiliy Lector <vasiliy.lector@gmail.com>
2016-08-22 23:02:50 +0300

    caret in input

--- components/input/Input.js
+++ components/input/Input.js
@@ -1,7 +1,56 @@
-import { html } from 'rerender';
+import { Component, html } from 'rerender';
 
-function Input(props) {
-    return html `<input _=${props} />`;
+class Input extends Component {
+    handleSetRef(domNode) {
+        if (typeof this.lastValue !== 'undefined') {
+            domNode.value = this.lastValue;
+            if (this.focused) {
+                let defaultCaret = this.lastValue.length,
+                    {
+                        lastSelectionStart = defaultCaret,
+                        lastSelectionEnd = defaultCaret,
+                        lastSelectionDirection = 'none'
+                    } = this;
+
+                domNode.focus();
+                domNode.setSelectionRange(lastSelectionStart, lastSelectionEnd, lastSelectionDirection);
+            }
+        }
+    }
+
+    handleFocus() {
+        this.focused = true;
+    }
+
+    handleBlur() {
+        this.focused = false;
+        this.lastSelectionStart = undefined;
+        this.lastSelectionEnd = undefined;
+        this.lastSelectionDirection = undefined;
+    }
+
+    handleInput(event) {
+        this.lastValue = event.target.value;
+        this.props.onInput && this.props.onInput(event);
+    }
+
+    saveCaret(event) {
+        this.lastSelectionStart = event.target.selectionStart;
+        this.lastSelectionEnd = event.target.selectionEnd;
+        this.lastSelectionDirection = event.target.selectionDirection;
+    }
+
+    render() {
+        return html `<input _=${this.props}
+            onInput=${this.handleInput}
+            onBlur=${this.handleBlur}
+            onFocus=${this.handleFocus}
+            onKeyUp=${this.saveCaret}
+            onMouseUp=${this.saveCaret}
+            onSetRef=${this.handleSetRef} />`;
+    }
 }
+
+Input.autoBind = ['handleInput', 'handleSetRef', 'handleFocus', 'handleBlur', 'saveCaret'];
 
 export default Input;
