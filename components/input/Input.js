38b0dfb591c663ad808288998b8755e481190839
Vasiliy Lector <vasiliy.lector@gmail.com>
2016-08-27 09:32:57 +0300

    one input

--- components/input/Input.js
+++ components/input/Input.js
@@ -1,21 +1,79 @@
-import { html } from 'rerender';
-import InputText from './InputText';
-import InputCheckbox from './InputCheckbox';
-import InputRadio from './InputRadio';
-import InputDefault from './InputDefault';
+import { Component, html } from 'rerender';
 
-const inputsByType = {
-    text: InputText,
-    checkbox: InputCheckbox,
-    radio: InputRadio,
-    default: InputDefault
-};
+const
+    PROPS_TO_OMIT = {
+        checked: true,
+        forceUpdate: true,
+        value: true
+    };
 
-function Input(props, children) {
-    const { type = 'text' } = props,
-        InputOfType = inputsByType[type] || inputsByType['default'];
+class Input extends Component {
 
-    return html `<instance of=${InputOfType} _=${props}>${children}</instance>`;
+    componentWillMount() {
+        this.setStateByProps();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setStateByProps(nextProps);
+    }
+
+    handleChange(event) {
+        this.changed = true;
+
+        let { checked } = event.target;
+
+        this.setState({
+            checked
+        });
+
+        this.props.handleChange && this.props.handleChange(event);
+    }
+
+    handleInput(event) {
+        this.changed = true;
+
+        let { value } = event.target;
+
+        this.setState({
+            value
+        });
+
+        this.props.handleInput && this.props.handleInput(event);
+    }
+
+    setStateByProps(props = this.props) {
+        let nextState = Object.keys(props).reduce((memo, key) => {
+                if (!PROPS_TO_OMIT[key]) {
+                    memo[key] = props[key];
+                }
+
+                return memo;
+            }, {}),
+            { value, forceUpdate, checked } = props;
+
+        this.defaultValue = value;
+
+        if (!this.changed || forceUpdate) {
+            if (typeof value !== 'undefined') {
+                nextState.value = value;
+            }
+
+            if (typeof checked !== 'undefined') {
+                nextState.checked = checked;
+            }
+        }
+
+        this.setState(nextState);
+    }
+
+    render() {
+        return html `<input _=${this.state}
+            onInput=${this.handleInput}
+            onChange=${this.handleChange}
+        />`;
+    }
 }
+
+Input.autoBind = ['handleInput', 'handleChange'];
 
 export default Input;
