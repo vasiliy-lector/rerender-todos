d9574e8fe08661a1f08066a42db2b9435de4bf9f
Vasiliy Lector <vasiliy.lector@gmail.com>
2017-06-13 08:39:37 +0300

    [mask] => mask

--- pages/Controllers.js
+++ pages/Controllers.js
@@ -3,11 +3,11 @@ import { mask } from '../components/maskController/MaskController';
 
 function ControllersPage(){
     return jsx `<input
-            type='text'
-            controller=${[mask]}
-            mask='+7 (___) ___-__-__'
-            value='9251112233'
-        />`;
+        type='text'
+        controller=${mask}
+        mask='+7 (___) ___-__-__'
+        value='9251112233'
+    />`;
 }
 
 export default ControllersPage;
